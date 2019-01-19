f769d566e6ac4bdf679e906841598760e1bf031d
Your name <myEmail@Gmail.com>
2019-01-19 12:43:14 -0500

    Add subtract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static double add(double num1, double num2){
+        return + num2;
+    }
+
+    public static double subtract(double num1, double num2){
+        return num1 - num2;
+    }
 }
